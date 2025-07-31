50b0d2236bb5d58c3ed5e3148dbe68b38a8127f3
FORDIDODI <tibbitarigan@gmail.com>
2025-07-31 23:07:06 +0700

    Update sebelum tidur 😪😪

---
+++ app/src/main/java/com/example/simplecal/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.simplecal
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Devide: CalculatorOperation("/")
+}
--- app/src/main/java/com/example/simplecal/MainActivity.kt
+++ app/src/main/java/com/example/simplecal/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.simplecal.ui.theme.SimpleCalTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleCalTheme {
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
             }
         }
     }
