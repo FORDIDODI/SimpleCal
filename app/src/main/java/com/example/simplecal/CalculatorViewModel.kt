108184a6c1adaebd42c449cd2c60e56f3dbc78af
FORDIDODI <tibbitarigan@gmail.com>
2025-08-01 14:40:07 +0700

    base completed

--- app/src/main/java/com/example/simplecal/CalculatorViewModel.kt
+++ app/src/main/java/com/example/simplecal/CalculatorViewModel.kt
@@ -33,11 +33,32 @@ class CalculatorViewModel: ViewModel() {
             state.number1.isNotBlank() -> state = state.copy(
                 number1 = state.number1.dropLast(1)
             )
+            state.operation != null -> state = state.copy(
+                operation = null
+            )
+            state.number1.isNotBlank() -> state = state.copy(
+                number1 = state.number1.dropLast(1)
+            )
         }
     }
 
     private fun performCalculation() {
-        TODO("Not yet implemented")
+        val number1 = state.number1.toDoubleOrNull()
+        val number2 = state.number2.toDoubleOrNull()
+        if (number1 != null && number2 != null) {
+        val result = when(state.operation) {
+            is CalculatorOperation.Add -> number1 + number2
+            is CalculatorOperation.Subtract -> number1 - number2
+            is CalculatorOperation.Multiply -> number1 * number2
+            is CalculatorOperation.Divide -> number1 / number2
+            null -> return
+        }
+            state = state.copy(
+                number1 = result.toString().take(15),
+                number2 = "",
+                operation = null
+            )
+        }
     }
 
     private fun enterOperation(operation: CalculatorOperation) {
@@ -47,12 +68,40 @@ class CalculatorViewModel: ViewModel() {
     }
 
     private fun enterDecimal() {
-        TODO("Not yet implemented")
+       if (state.operation == null && !state.number1.contains(".")
+           && state.number1.isNotBlank()) {
+           state = state.copy(
+               number1 = state.number1 + "."
+           )
+           return
+       }
+        if (!state.number2.contains(".") && state.number2.isNotBlank()) {
+            state = state.copy(
+                number1 = state.number2 + "."
+            )
+        }
     }
 
     private fun enterNumber(number: Int) {
-        
+        if (state.operation == null) {
+            if (state.number1.length >= MAX_NUM_LENGTH) {
+                return
+            }
+            state = state.copy(
+                number1 = state.number1 + number
+            )
+            return
+        }
+        if (state.number2.length >= MAX_NUM_LENGTH) {
+            return
+        }
+        state = state.copy(
+            number2 = state.number2 + number
+        )
     }
 
+    companion object {
+        private const val MAX_NUM_LENGTH = 8
+    }
 
 }
